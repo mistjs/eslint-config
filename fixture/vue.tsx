69fb7dbfe67138394e160bf20eaf4dcf3d606145
aibayanyu <aibayanyu@qq.com>
2023-02-01 22:12:30 +0800

    feat: add single attr

---
+++ fixture/vue.tsx
@@ -0,0 +1,21 @@
+const aa = () => {
+  const b = false
+  if (!b) {
+    console.log('A')
+  }
+  return () => {
+    return (
+      <n-config-provider
+        locale="naiveLocale.locale"
+        date-locale="naiveLocale.dateLocale"
+        theme="layoutTheme"
+        theme-overrides="overridesTheme"
+      >
+        <n-global-style />
+        <app-provider>
+          <router-view />
+        </app-provider>
+      </n-config-provider>
+    )
+  }
+}
--- packages/vue-jsx/index.js
+++ packages/vue-jsx/index.js
@@ -1,23 +1,22 @@
 module.exports = {
-    extends: [
-        "@mistjs/eslint-config-vue",
-        "@vue/eslint-config-prettier"
-    ],
-    rules:{
-        "prettier/prettier": [
-            "warn",
-            {
-                "singleQuote": true,
-                "semi": false,
-                "printWidth": 200,
-                "trailingComma": "none",
-                "bracketSpacing": true,
-                "jsxBracketSameLine": false,
-                "arrowParens": "avoid"
-            },
-            {
-                "usePrettierrc": false
-            }
-        ]
-    }
+  extends: ['@mistjs/eslint-config-vue', '@vue/eslint-config-prettier'],
+  rules: {
+    'prettier/prettier': [
+      'warn',
+      {
+        singleQuote: true,
+        semi: false,
+        printWidth: 200,
+        trailingComma: 'none',
+        bracketSpacing: true,
+        jsxBracketSameLine: false,
+        arrowParens: 'avoid',
+        singleAttributePerLine: true,
+        vueIndentScriptAndStyle: true
+      },
+      {
+        usePrettierrc: false
+      }
+    ]
+  }
 }
