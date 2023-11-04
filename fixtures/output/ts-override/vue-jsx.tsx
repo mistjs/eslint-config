4630ff3d0524dc3f6df6cd36dde79386b7c5489d
aibayanyu <aibayanyu@qq.com>
2023-11-04 16:20:32 +0800

    feat: update test

--- fixtures/output/ts-override/vue-jsx.tsx
+++ fixtures/output/ts-override/vue-jsx.tsx
@@ -2,22 +2,25 @@ import { defineComponent } from 'vue'
 
 export default defineComponent(() => {
   return () => {
-    return <div>
-            <div>Test</div>
-            <button>测试
-            <p>撒旦撒</p>
-                <a href="/">asds</a>
-            </button>
-        </div>
+    return (
+      <div>
+        <div>Test</div>
+        <button>
+          测试
+          <p>撒旦撒</p>
+          <a href="/">asds</a>
+        </button>
+      </div>
+    )
   }
 })
 
 export const Comp = defineComponent(() => {
   return () => {
     return (
-            <div>
-                测试
-            </div>
+      <div>
+        测试
+      </div>
     )
   }
 })
